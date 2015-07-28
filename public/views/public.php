31273239d35006bcfceeeabda23979d532f68fbf
ChrisMKindred <ChrisMKindred@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-28 18:41:14 +0000

    Tested with WP 4.2.3
    git-svn-id: https://plugins.svn.wordpress.org/usgs-stream-flow-data/tags/2.2.1@1208516 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ public/views/public.php
@@ -0,0 +1,11 @@
+<?php
+/** 
+ * @package   USGS Steam Flow Data
+ * @author    Chris Kindred <Chris@kindredwebconsulting.com>
+ * @license   GPL-2.0+
+ * @link      http://www.kindredwebconsulting.com
+ * @copyright 2015 Kindred Web Consulting
+ */
+?>
+
+<!-- This file is used to markup the public facing aspect of the plugin. -->
